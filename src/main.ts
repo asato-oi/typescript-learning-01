2a9024feb5a9455c5b3098b7cc48fccf8d65d40c
Asato Ōi <83337845+asato-oi@users.noreply.github.com>
2025-02-10 22:52:18 +0900

    ✨ シングルトンによるグローバルステートを定義

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
+/** Project State Management */
+class ProjectState {
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addProject(title: string, description: string, manDay: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      manDay: manDay,
+    };
+
+    this.projects.push(newProject);
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 /** validate input value */
 interface Validatable {
   value: string | number;
